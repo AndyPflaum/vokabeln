cdcfdebbc7ef5aee5b5c20a7bf5a48f4baeb6d09
Andy Pflaum <andyreas.pa@gmail.com>
2024-12-18 19:20:59 +0100

    Added button for insert vocabulary

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,13 @@ import { VokabelnFragenComponent } from '../vokabeln-fragen/vokabeln-fragen.comp
 import { CorrectAndIncorrectComponent } from '../correct-and-incorrect/correct-and-incorrect.component';
 import { VokabelnService } from '../vokabeln.service';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [VokabelnFragenComponent, CorrectAndIncorrectComponent, CommonModule],
+  imports: [VokabelnFragenComponent, CorrectAndIncorrectComponent, CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
